81c08e194fb2df486f3c5f4ec407b83dd55b38fd
nrshagor <noorerabbishagor@gmail.com>
2025-09-20 12:32:51 +0600

    feat(auth, categories, products, users): implement modules with controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, UsersModule, ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
